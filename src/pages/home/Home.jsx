f447af5963e97249a9ee20fd311bca0cd6ee237d
Zafar Khan <65030364+Zafarkhan06@users.noreply.github.com>
2024-05-30 16:08:45 +0500

    smalll changes in 3rd portion

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,7 +4,10 @@ import truck from "../../asset/images/first-truck.webp";
 import usaFlag from "../../asset/images/usa-flag.webp";
 import SvgComponent from "../components/SvgComponent";
 import capPerson from "../../asset/images/cap-person.webp";
-
+import threeTrucks from "../../asset/images/three-trucks.webp"
+import FolderCopyIcon from '@mui/icons-material/FolderCopy';
+import HandshakeIcon from '@mui/icons-material/Handshake';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 function Home() {
   return (
     <>
@@ -114,9 +117,8 @@ function Home() {
                 <SvgComponent className="absolute -bottom-3 right-10 w-32  opacity-60 " />
               </div>
               <div className="truck-bg rounded-2xl shadow-2xl mt-20 realtive">
-                
                 <div className="py-20 px-6 md:px-20 flex flex-col  md:items-end">
-                
+
                   <p className="md:text-3xl text-2xl lg:text-4xl font-bold font-Raleway text-center md:text-right italic text-white">Call us today, get loaded <br /> & be profitable now
                   </p>
                   <div className="mt-6 flex justify-center md:justify-start lg:mt-10">
@@ -142,11 +144,12 @@ function Home() {
                     }}> Call Now!</Button>
                   </div>
                 </div>
+
               </div>
             </Grid>
           </Grid>
         </Container>
-        <div className="flex py-20 justify-center mt-20 bg-[#f0dac2] items-start">
+        <div className="flex py-20 md:py-32 justify-center mt-20 bg-[#f0dac2] items-start">
           <Container
             disableGutters={true}
             maxWidth="lg"
@@ -154,19 +157,165 @@ function Home() {
           >
             <Grid container sx={{ justifyContent: "center" }}>
               <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <div className="w-1/4">
-                  <Rating name="read-only" value={5} readOnly />
-                  <p className="md:text-base text-sm font-bold lg:text-xl font-Raleway text-center md:text-start text-[#E87700] ">Lopez Hauling Inc  </p>
-                  <p className="text-[#E87700] text-sm ">Our company, Lopez Hauling, just started back in July of 2021 and Genius Dispatch has been with us since the beginning. They have been so helpful with us being a start up company and not knowing much about the industry to knowing where the money is and what we need to do to get it! Jessica, especially has been so amazing and her along with many others are very quick to respond and always take care of any issue we run into. To the point I don’t have to worry about much but making money!
-                  </p>
+                <div className="flex justify-center gap-8 md:gap-16 items-start flex-col lg:flex-row">
+                  <div className="w-full lg:w-1/4">
+                    <Rating name="read-only" value={5} readOnly />
+                    <p className="md:text-base text-sm font-bold lg:text-lg font-Raleway text-start text-[#E87700]">
+                      Lopez Hauling Inc
+                    </p>
+                    <p className="text-[#E87700] text-xs md:text-sm">
+                      Our company, Lopez Hauling, just started back in July of 2021 and Genius Dispatch has been with us since the beginning. They have been so helpful with us being a start-up company and not knowing much about the industry to knowing where the money is and what we need to do to get it! Jessica, especially has been so amazing and her along with many others are very quick to respond and always take care of any issue we run into. To the point I don’t have to worry about much but making money!
+                    </p>
+                  </div>
+                  <div className="w-full lg:w-1/4">
+                    <Rating name="read-only" value={5} readOnly />
+                    <p className="md:text-base text-sm font-bold lg:text-lg font-Raleway  text-start text-[#E87700]">
+                      Spartan Cargo LLC
+                    </p>
+                    <p className="text-[#E87700] text-xs md:text-sm">
+                      I can’t say enough to express my positive experience with this dispatch service. With the challenges of expenses going through the roof these days, having support from a dedicated staff is very important.
+                    </p>
+                  </div>
+                  <div className="w-full lg:w-1/4">
+                    <Rating name="read-only" value={5} readOnly />
+                    <p className="md:text-base text-sm font-bold lg:text-lg font-Raleway text-start text-[#E87700]">
+                      National Brothers Inc
+                    </p>
+                    <p className="text-[#E87700] text-xs md:text-sm">
+                      Our trucking company was in need of a dispatching company to dispatch our fleet of trucks and found Genius Dispatching in an article written about top dispatching companies. We have only worked with Genius for a short time but thus far, they have not disappointed. David and Jessica have both been readily available to answer questions our members have regarding their process. They have been nothing but professional and are quick to reach out to our team about concerns and/or issues. We look forward to continuing a great profitable relationship with their team.
+                    </p>
+                  </div>
+                  <div className="w-full lg:w-1/4">
+                    <Rating name="read-only" value={5} readOnly />
+                    <p className="md:text-base text-sm font-bold lg:text-lg font-Raleway text-start text-[#E87700]">
+                      Venados Logistics LLC
+                    </p>
+                    <p className="text-[#E87700] text-xs md:text-sm">
+                      Excellent service that they provide in all aspects, Elisa who is my dispatch assigned always on the lookout for having the best-paid loads, always waiting for me to be loaded and unloaded on time and also pending along with Jessica who always pays us the total on time of the load plus the extra charges that we have for lumper or detention.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="relative flex justify-center">
+                  <SvgComponent className="absolute left-0 w-36 top-10 opacity-60 " />
+                  <LazyLoadImage
+                    src={threeTrucks}
+                    alt="muntazim logo"
+                    className="w-full absolute top-0 md:w-full lg:w-3/6 z-20"
+                  />
+                  <SvgComponent className="absolute right-0 w-36 top-52 opacity-30 " />
                 </div>
               </Grid>
             </Grid>
           </Container>
         </div>
+        <Container
+          disableGutters={true}
+          maxWidth="lg"
+          sx={{ px: { xs: "10px", lg: "0px" } }}
+        >
+          <Grid container sx={{ justifyContent: "center" }}>
+            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+              <div className="lg:mt-44 md:mt-60 mt-36 flex flex-col justify-center items-center">
+                <p className="md:text-3xl text-2xl lg:text-4xl font-bold font-Raleway text-center  italic text-[#292929]">Getting started is easy
+                </p>
+                <div className="mt-16 flex flex-col md:flex-row gap-10 lg:gap-28 items-start">
+                  <div className="flex justify-center items-center flex-col">
+                    <FolderCopyIcon sx={{ fontSize: "70px", color: "#ff914c" }} />
+                    <p className="md:text-xl text-lg mt-5 mb-4 lg:text-2xl font-semibold font-Raleway text-center   text-[#292929]">Send Documents
+                    </p>
+                    <p className="text-lg font-Raleway text-center  italic text-[#292929]">MC Authority, Certificate of <br /> Insurance and W-9 Form
+                    </p>
+                    <div className="mt-6 flex justify-center md:justify-start lg:mt-10">
+                      <Button variant="contained" className="font-Raleway" sx={{
 
+                        py: 1,
+                        boxShadow: "none",
+                        fontSize: { sm: "0.75rem", md: "1rem", lg: "1rem", xl: "1rem" },
+                        fontWeight: "bold",
 
+                        letterSpacing: "0px",
+                        fontFamily: "Raleway",
+                        textTransform: "none",
+                        px: 6,
+                        background:
+                          "#ff914c",
+                        color: "white",
+                        "&:hover": {
+                          background:
+                            "#ff914c",
+                          boxShadow: "none",
+                        },
+                      }}> Send Documents</Button>
+                    </div>
+                  </div>
+                  <div className="flex justify-center items-center flex-col">
+                    <HandshakeIcon sx={{ fontSize: "70px", color: "#ff914c" }} />
+                    <p className="md:text-xl text-lg mt-5 mb-4 lg:text-2xl font-semibold font-Raleway text-center   text-[#292929]">Sign Agreement
+                    </p>
+                    <p className="text-lg font-Raleway text-center  italic text-[#292929]">Sign a quick and easy dispatch <br /> service level agreement.
 
+                    </p>
+                    <div className="mt-6 flex justify-center md:justify-start lg:mt-10">
+                      <Button variant="contained" className="font-Raleway" sx={{
+
+                        py: 1,
+                        boxShadow: "none",
+                        fontSize: { sm: "0.75rem", md: "1rem", lg: "1rem", xl: "1rem" },
+                        fontWeight: "bold",
+
+                        letterSpacing: "0px",
+                        fontFamily: "Raleway",
+                        textTransform: "none",
+                        px: 6,
+                        background:
+                          "#ff914c",
+                        color: "white",
+                        "&:hover": {
+                          background:
+                            "#ff914c",
+                          boxShadow: "none",
+                        },
+                      }}> Request Agreement</Button>
+                    </div>
+                  </div>
+                  <div className="flex justify-center items-center flex-col">
+                    <LocalShippingIcon sx={{ fontSize: "70px", color: "#ff914c" }} />
+                    <p className="md:text-xl text-lg mt-5 mb-4 lg:text-2xl font-semibold font-Raleway text-center  text-[#292929]">Hit the road
+
+                    </p>
+                    <p className="text-lg font-Raleway text-center  italic text-[#292929]">We start dispatching<br /> your truck immediately.
+                    </p>
+                    <div className="mt-6 flex justify-center md:justify-start lg:mt-10">
+                      <Button variant="contained" className="font-Raleway" sx={{
+                        py: 1,
+                        boxShadow: "none",
+                        fontSize: { sm: "0.75rem", md: "1rem", lg: "1rem", xl: "1rem" },
+                        fontWeight: "bold",
+
+                        letterSpacing: "0px",
+                        fontFamily: "Raleway",
+                        textTransform: "none",
+                        px: 6,
+                        background:
+                          "#ff914c",
+                        color: "white",
+                        "&:hover": {
+                          background:
+                            "#ff914c",
+                          boxShadow: "none",
+                        },
+                      }}> Hire a Dispatcher</Button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div>
+                
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
       </div>
     </>
   );

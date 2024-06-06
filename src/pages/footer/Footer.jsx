198a44de3c65bce0ad0bc21b3f03b0bf4d265e31
Zafar Khan <65030364+Zafarkhan06@users.noreply.github.com>
2024-06-07 00:59:29 +0500

    df

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -6,6 +6,8 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import docusign from "../../asset/images/docu-sign.png";
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import SubscriptionsRoundedIcon from '@mui/icons-material/SubscriptionsRounded';
+import truckstop from "../../asset/images/truckkstop.png";
+import dat from "../../asset/images/2560px-DAT-Solutions-Logo.svg.png";
 function Footer() {
   const navigate = useNavigate();
   return (
@@ -65,16 +67,23 @@ function Footer() {
               </div>
             </div>
             <div className="flex justify-center md:justify-between  items-center flex-col md:flex-row mt-10">
-              <div className="flex w-full md:w-1/4 items-center justify-center md:justify-start">
+              <div className="flex flex-col  w-full md:w-1/4 items-center justify-center md:justify-start">
                 <div className="flex justify-center md:justify-start mb-6">
                   <LazyLoadImage onClick={() => navigate("/")} src={logo} alt="muntazim logo" className="rounded-full w-3/12 cursor-pointer" />
                 </div>
-
+                <div className="flex justify-center md:justify-start mb-6">
+                  <LazyLoadImage src={truckstop} alt="muntazim logo" className="rounded-full w-7/12 cursor-pointer" />
+                </div>
+                <div className="flex justify-center md:justify-start mb-6">
+                  <LazyLoadImage src={dat} alt="muntazim logo" className="rounded-full w-7/12 cursor-pointer" />
+                </div>
               </div>
               <div>
-                <p className="hover:text-[#cc3333] cursor-pointer text-center md:text-right font-Raleway text-white">Email: info@perfectgloballogistics.com</p>
-                <p className="hover:text-[#cc3333] cursor-pointer font-Raleway text-white text-center md:text-right mt-3">Phone: +1 (844) 264-4453</p>
-
+                <a href="mailto:info@perfectgloballogistics.com">
+                  <p className="hover:text-[#cc3333] cursor-pointer text-center md:text-right font-Raleway text-white">info@perfectgloballogistics.com</p></a>
+                <a href="tel:+1(844)264-4453">
+                  <p className="hover:text-[#cc3333] cursor-pointer font-Raleway text-white text-center md:text-right mt-3">(844) 264-4453</p>
+                </a>
               </div>
             </div>
             <div className="flex justify-between md:flex-row flex-col items-center">
@@ -94,12 +103,12 @@ function Footer() {
               </div>
               <div>
                 <p className="hover:text-[#cc3333] text-center md:text-right cursor-pointer font-Raleway text-white mt-3">Working Hours: 24 hours</p>
-                <p className="hover:text-[#cc3333] text-center md:text-right cursor-pointer font-Raleway text-white mt-3">​Dispatchers: 24/7</p>
+                {/* <p className="hover:text-[#cc3333] text-center md:text-right cursor-pointer font-Raleway text-white mt-3">​Dispatchers: 24/7</p> */}
               </div>
             </div>
-            <hr  className="mt-5"/>
+            <hr className="mt-5" />
             <div className="flex justify-center items-center py-4">
-              
+
               <div className=" flex flex-wrap gap-8">
                 <motion.p
                   whileInView={{
